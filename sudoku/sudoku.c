6f01a51663433fe55723b62bb419a622ee57087c
crystalYoo99 <21800464@handong.edu>
2019-10-02 03:57:47 +0900

    dd

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -1,28 +1,70 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main ()
+int main (int argc, char** argv)
 {
-        FILE * f = fopen("origin_sudoku", "r");
+        FILE * f = fopen(argv[1], "r");
     FILE * fp = fopen("formula", "w") ;
 
-        int x, y;
-    char input[9][9];
+        int x=0, y=0;
+	char input[9][9];
+char str[100];
     int star_addr[9];
     int num[81];
     int num_addr[81];
     int count_num=0;
     int count_star=0;
-
+	int index = 0;
+	char buffer[100];
+	int col=0, row=0;
     //원본 파일에서 줄로 읽어오기
-    for (int i = 0; i<9; i++){
+/*    for (int i = 0; i<9; i++){
         fscanf(f, "%s", input[i]);
-        printf("%s\n",input[i]);
-        }
+    }*/
+/*	while(fgets(str, 100, f) != NULL;
+	str[strlen(str)-1] = 0;
+	if(str[0] == 0) break;
+	row++;
+	token = strtok(ULL, " ");*/
     //변수 만들기
         for (y = 1 ; y <= 9 ; y++)
                 for (x = 1 ; x <= 9 ; x++)
                         fprintf(fp, "(declare-const a%d%d Int)\n", y, x) ;
-
+	//
+	/*for(y=0; y<9; y++) {
+		while(input[y][index] != '\n'){
+			if(input[index][y] == '*') {
+				star_addr[count_star] = (x+1)*10 + (y+1);
+				count_star++;
+				x++;
+			}
+			else if(input[y][index] == ' ') {}
+			else if(input[y][index] == '?') {
+				x++;
+			}
+			else {
+				num[count_num] = input[y][index]-'0';
+				num_addr[count_num] = (x+1)*10+y+1;
+				x++;
+			}
+		index++;
+}}*/
+	while(1) {
+		fgets(buffer, sizeof(buffer), f);
+		if(feof(f)) break;
+		row++;
+		col=0;
+		for(int i = 0; col<9; i++) {
+			if(!(buffer[i] == ' ') || (!(buffer[i] == '\n'))) {
+				col++;
+				input[row-1][col-1] = buffer[i];
+			}
+			
+		}
+	//	col++;
+	}
+	
     //월본에서 별과 숫자 위치 저장
     for(y=0; y<9; y++) {
         for(x=0; x<9; x++) {
@@ -31,13 +73,15 @@ int main ()
                 count_star++;
             }
             else if(input[x][y] == '?') {}
-            else {
+            else if(input[x][y] == ' ') {}
+		else {
                 num[count_num] = input[x][y]-'0';
-                num_addr[count_num] = (x+1)*10 +y+1;
+                num_addr[count_num] = (x+1)*10 +(y+1);
                 count_num++;
             }
         }
     }
+
     num[count_num] = '\0';
     num_addr[count_num] = '\0';
     star_addr[count_star] = '\0';
@@ -83,12 +127,13 @@ int main ()
     }
     fprintf(fp,"\n");
     //별끼리 달라야함
-        fprintf(fp, "(assert (distinct");
+	if(count_star!=0){
+		fprintf(fp, "(assert (distinct");
     for(int i = 0; i < count_star; i++) {
             fprintf(fp, " a%d%d", star_addr[i]/10, star_addr[i]%10);
     }
         fprintf(fp, "))\n");
-
+}
 
         fprintf(fp, "(check-sat)\n(get-model)\n") ;
 
@@ -112,12 +157,12 @@ FILE * fin = popen("z3 formula", "r") ; //FIXME
         char b[128] ;
         char s[128];
         char t[128];
-        fscanf(fin, "%s %s", b, b) ;
+        fscanf(fin, "%s %s", b, s) ;
+	if(strcmp(b,"unsat")==0){ printf("No solution!\n"); exit(-1);}
         for(k =0; k < 81; k++) {
                 fscanf(fin, "%s %s %s %s %s", b, s, b, b, t);
                 i = s[1] - '0';
                 j = s[2] - '0';
-
                 board[i][j] = t[0] - '0';
         }
         for(i = 1; i <= 9; i++) {
